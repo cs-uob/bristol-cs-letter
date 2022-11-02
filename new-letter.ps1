3163050402cae7c4b1261ee8c07f04b01465ecab
Alex Kavvos <alex.kavvos@bristol.ac.uk>
2022-11-02 21:48:55 +0000

    script update

--- new-letter.ps1
+++ new-letter.ps1
@@ -6,9 +6,21 @@ $target = Join-Path -Path (Get-Item .).FullName -ChildPath $subdir
 if (Test-Path -Path $target) {
   throw "Directory already exists."
 }
-Write-Output "Creating new letter in $subdir..."
+Write-Output "Creating new letter in .\$subdir..."
 git clone --depth=1 git@github.com:cs-uob/bristol-cs-letter.git $target 2> $null
-$gitDir=Join-Path -Path $target -ChildPath ".git"
-if (Test-Path -Path $gitDir) {
-  Remove-Item -LiteralPath $gitDir -Force -Recurse
+
+$repoFiles = @(
+  (Join-Path -Path $target -ChildPath ".git"),
+  (Join-Path -Path $target -ChildPath ".gitmodules"),
+  (Join-Path -Path $target -ChildPath "new-letter.ps1")
+)
+foreach ($f in $repoFiles) {
+  if (Test-Path -Path $f) {
+    Remove-Item -LiteralPath $f -Force -Recurse
+  }
+}
+
+$gitignore = Join-Path -Path $target -ChildPath ".gitignore"
+if (Test-Path -Path $gitignore) {
+  Add-Content $gitignore "`n/helvetica-neue"
 }
